04eb46159f693ace33fac8701e78c2df263eecc8
Song Qiang <songqiang@users.noreply.github.com>
2019-05-11 19:49:11 -0700

    Update jupyter.sh

--- jupyter.sh
+++ jupyter.sh
@@ -5,3 +5,6 @@ jupyter labextension install @jupyter-widgets/jupyterlab-manager
 
 # add table of contents
 jupyter labextension install @jupyterlab/toc
+
+# variableInspector: https://github.com/lckr/jupyterlab-variableInspector
+jupyter labextension install @lckr/jupyterlab_variableinspector
